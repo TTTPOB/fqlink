a1b963b10d64d80183a09fbba1f98fb21bc31019
yujw <yu-jw20@mails.tsinghua.edu.cn>
2023-04-28 21:14:19 +0800

    feat: add ability to set download name

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -11,20 +11,41 @@ pub enum AccessionCodes {
 }
 
 #[derive(Debug, Clone)]
-pub struct Srx(String);
+pub struct Srx {
+    acc: String,
+    name: Option<String>,
+}
 #[derive(Debug, Clone)]
-pub struct Srr(String);
+pub struct Srr {
+    acc: String,
+    name: Option<String>,
+}
 #[derive(Debug, Clone)]
-pub struct Gsm(String);
+pub struct Gsm {
+    acc: String,
+    name: Option<String>,
+}
 
 impl AccessionCodes {
     pub fn from_str(s: &str) -> Result<Self> {
-        let prefix = &s[..3];
-        match prefix {
-            "SRX" => Ok(AccessionCodes::Srx(Srx(s.to_string()))),
-            "SRR" => Ok(AccessionCodes::Srr(Srr(s.to_string()))),
-            "GSM" => Ok(AccessionCodes::Gsm(Gsm(s.to_string()))),
-            _ => Err(anyhow::anyhow!("unknown prefix: {}", prefix)),
+        // any number of space as delimeter
+        let mut iter = s.split_whitespace();
+        let acc = iter.next().expect("malformed input").to_string();
+
+        let name = match iter.next() {
+            Some(name) => Some(name.to_string()),
+            None => None,
+        };
+        if let Some(_) = iter.next() {
+            panic!("malformed input, too many fields (maybe you have whitespace in name)");
+        }
+        let prefix = &acc[..3];
+        // case-insensitive
+        match prefix.to_lowercase().as_str() {
+            "srx" => Ok(AccessionCodes::Srx(Srx { acc, name })),
+            "srr" => Ok(AccessionCodes::Srr(Srr { acc, name })),
+            "gsm" => Ok(AccessionCodes::Gsm(Gsm { acc, name })),
+            _ => panic!("malformed input"),
         }
     }
 }
@@ -42,14 +63,47 @@ pub struct EnaRecord {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub struct DownloadInfo {
+    pub name: Option<String>,
     pub orig_acc: String,
     pub run_acc: String,
     pub http_url: String,
     pub md5: String,
     pub ascp_url: String,
+    pub download_path: Option<String>,
 }
 
 impl DownloadInfo {
+    fn new(
+        name: Option<String>,
+        orig_acc: String,
+        run_acc: String,
+        http_url: String,
+        md5: String,
+        ascp_url: String,
+    ) -> Self {
+        let mut info = Self{
+            name,
+            orig_acc,
+            run_acc,
+            http_url,
+            md5,
+            ascp_url,
+            download_path: None,
+        };
+        info.download_path = Some(info.download_path());
+        info
+    }
+    fn download_path(&self) -> String {
+        let file_stem = match &self.name {
+            Some(name) => {
+                name.clone() + "/" + self.http_url.split('/').last().unwrap()
+            },
+            None => {
+                self.orig_acc.clone() + "/" + self.run_acc.as_str() + "/" + self.http_url.split('/').last().unwrap()
+            }
+        };
+        file_stem + ".fastq.gz"
+    }
     pub fn to_aria2(&self) -> String {
         let mut aria2 = String::new();
         aria2.push_str(format!("https://{}", self.http_url).as_str());
@@ -58,7 +112,13 @@ impl DownloadInfo {
         aria2.push_str("\n ");
         aria2.push_str("check-integrity=true");
         aria2.push_str("\n ");
-        aria2.push_str(format!("out={}.fastq.gz", self.orig_acc).as_str());
+        aria2.push_str(
+            format!(
+                "out={}",
+                self.download_path()
+            )
+            .as_str(),
+        );
         aria2.push_str("\n");
         aria2
     }
@@ -71,6 +131,7 @@ impl DownloadInfo {
 #[async_trait]
 pub trait DownloadableAccession {
     fn orig_accession(&self) -> String;
+    fn name(&self) -> Option<String>;
     async fn get_downloadable_accession(&self) -> Option<String> {
         Some(self.orig_accession())
     }
@@ -104,17 +165,21 @@ pub trait DownloadableAccession {
                     record.fastq_md5.split(";").map(|x| x.to_string()).collect();
                 let http_url_vec: Vec<String> =
                     record.fastq_ftp.split(";").map(|x| x.to_string()).collect();
-                let ascp_url_vec: Vec<String> =
-                    record.fastq_aspera.split(";").map(|x| x.to_string()).collect();
+                let ascp_url_vec: Vec<String> = record
+                    .fastq_aspera
+                    .split(";")
+                    .map(|x| x.to_string())
+                    .collect();
                 let run_acc_: String = record.run_accession.to_string();
                 for (md5, http_url, ascp_url) in izip!(md5_vec, http_url_vec, ascp_url_vec) {
-                    let download_info = DownloadInfo {
-                        orig_acc: self.orig_accession().to_string(),
-                        run_acc: run_acc_.clone(),
-                        http_url: "https://".to_string() + &http_url,
-                        md5: md5,
-                        ascp_url: "era-fasp@".to_string() + &ascp_url,
-                    };
+                    let download_info = DownloadInfo::new(
+                        self.name(),
+                        self.orig_accession().to_string(),
+                        run_acc_.clone(),
+                        "https://".to_string() + &http_url,
+                        md5,
+                        "era-fasp@".to_string() + &ascp_url,
+                    );
                     download_info_list.push(download_info);
                 }
             }
@@ -127,20 +192,29 @@ pub trait DownloadableAccession {
 
 #[async_trait]
 impl DownloadableAccession for Srx {
+    fn name(&self) -> Option<String> {
+        self.name.clone()
+    }
     fn orig_accession(&self) -> String {
-        self.0.to_string()
+        self.acc.to_string()
     }
 }
 #[async_trait]
 impl DownloadableAccession for Srr {
+    fn name(&self) -> Option<String> {
+        self.name.clone()
+    }
     fn orig_accession(&self) -> String {
-        self.0.to_string()
+        self.acc.to_string()
     }
 }
 #[async_trait]
 impl DownloadableAccession for Gsm {
+    fn name(&self) -> Option<String> {
+        self.name.clone()
+    }
     fn orig_accession(&self) -> String {
-        self.0.to_string()
+        self.acc.to_string()
     }
     async fn get_downloadable_accession(&self) -> Option<String> {
         let client = reqwest::Client::new();
@@ -192,11 +266,18 @@ impl DownloadableAccession for Gsm {
 
 #[async_trait]
 impl DownloadableAccession for AccessionCodes {
+    fn name(&self) -> Option<String> {
+        match self {
+            AccessionCodes::Srx(srx) => srx.name.clone(),
+            AccessionCodes::Srr(srr) => srr.name.clone(),
+            AccessionCodes::Gsm(gsm) => gsm.name.clone(),
+        }
+    }
     fn orig_accession(&self) -> String {
         match self {
-            AccessionCodes::Srx(srx) => srx.0.to_string(),
-            AccessionCodes::Srr(srr) => srr.0.to_string(),
-            AccessionCodes::Gsm(gsm) => gsm.0.to_string(),
+            AccessionCodes::Srx(srx) => srx.acc.to_string(),
+            AccessionCodes::Srr(srr) => srr.acc.to_string(),
+            AccessionCodes::Gsm(gsm) => gsm.acc.to_string(),
         }
     }
     async fn get_downloadable_accession(&self) -> Option<String> {
@@ -220,7 +301,10 @@ mod tests {
     use super::*;
     #[tokio::test]
     async fn test_get_download_info_srr() {
-        let run_acc = AccessionCodes::Srr(Srr("SRR000001".to_string()));
+        let run_acc = AccessionCodes::Srr(Srr {
+            acc: "SRR000001".to_string(),
+            name: "".to_string().into(),
+        });
         let download_info_list = run_acc.get_download_info().await;
         println!("{:?}", download_info_list);
         let download_info_list = download_info_list.unwrap();
@@ -239,7 +323,10 @@ mod tests {
 
     #[tokio::test]
     async fn test_get_download_info_srx() {
-        let srx = AccessionCodes::Srx(Srx("SRX2243567".to_string()));
+        let srx = AccessionCodes::Srx(Srx {
+            acc: "SRX2243567".to_string(),
+            name: "".to_string().into(),
+        });
         let download_info_list = srx.get_download_info().await.unwrap();
         assert_eq!(download_info_list.len(), 1);
         assert_eq!(download_info_list[0].orig_acc, "SRX2243567");
@@ -256,7 +343,10 @@ mod tests {
 
     #[tokio::test]
     async fn test_get_download_info_gsm() {
-        let gsm = AccessionCodes::Gsm(Gsm("GSM2344754".to_string())); // in fact this is the same sample as above srx
+        let gsm = AccessionCodes::Gsm(Gsm {
+            acc: "GSM2344754".to_string(),
+            name: "".to_string().into(),
+        }); // in fact this is the same sample as above srx
         let download_info_list = gsm.get_download_info().await.unwrap();
         assert_eq!(download_info_list.len(), 1);
         assert_eq!(download_info_list[0].orig_acc, "GSM2344754");
